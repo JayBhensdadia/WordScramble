4c22ff8588636ee8c0f60a43c9c12f8d4b52fb44
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-04 10:58:36 +0530

    basic ui setup and adding new words to the list view

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                Section{
+                    TextField("Enter your word",text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section{
+                    ForEach(usedWords, id: \.self){word in
+                        HStack{
+                            Image(systemName: "\(word.count).circle.fill")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
-        .padding()
+        
+    }
+    func addNewWord(){
+        let trimmedWord = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        if trimmedWord.count <= 1{
+            return
+        }else{
+            withAnimation{
+                usedWords.insert(trimmedWord, at: 0)
+            }
+            newWord = ""
+        }
     }
 }
 
