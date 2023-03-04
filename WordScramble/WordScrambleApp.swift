9cbebef332595c213e2106cf046e899e0b91dd6c
Jay Bhensdadia <jayvpatel29@gmail.com>
2023-03-04 10:37:31 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jay Bhensdadia on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
